5fd2477d9ab00817f5a6b3c651d00f9c8a579fe0
Md Arifur Rahman Sojib <sojibkhanarif999@gmail.com>
2022-08-06 01:30:49 +0600

    break-continue

---
+++ break-continue.js
@@ -0,0 +1,65 @@
+
+// var items = [10, 20, 30, 40, 50, 60, 70, 80, 100, 120, 130, 151, 182, 200, 300]
+
+// var char = ['rahim', 'korim', 'jobbar', 'rasel', 'sojib', 'sirat', 'panna'];
+
+// for (var i = 0; i < char.length; i++) {
+//     var output = char[i];
+
+//     if (output == 'rasel') {
+//         break;
+
+//     }
+
+//     console.log(output);
+
+// }
+
+// var items = [10, 20, 30, 40, 50, 60, 70, 80, 100, 120, 130, 151, 182, 200, 300]
+
+// var char = ['rahim', 'korim', 'jobbar', 'rasel', 'sojib', 'sirat', 'panna'];
+
+// for (var i = 0; i < items.length; i++) {
+//     var output = items[i];
+
+//     if (output == '100') {
+//         break;
+
+//     }
+
+//     console.log(output);
+
+// }
+
+var items = [10, 20, 30, 40, 50, 60, 70, 80, 100, 120, 30, 151, 182, 20, 35]
+
+var char = ['rahim', 'korim', 'jobbar', 'rasel', 'sojib', 'sirat', 'panna'];
+
+for (var i = 0; i < items.length; i++) {
+    var output = items[i];
+
+    if (output > 100) {
+        break;
+
+    }
+
+    console.log(output);
+
+}
+
+// var items = [10, 20, 30, 40, 50, 60, 70, 80, 100, 120, 130, 151, 182, 200, 300]
+
+
+
+// for (var i = 0; i < items.length; i++) {
+//     var output = items[i];
+
+//     if (output > 60) {
+//         continue;
+
+//     }
+
+//     console.log(output);
+
+// }
+
