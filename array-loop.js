16c3a27f17616477358cb7968c3f79f235a5eab8
Md Arifur Rahman Sojib <sojibkhanarif999@gmail.com>
2022-08-06 00:28:52 +0600

    array-loop

---
+++ array-loop.js
@@ -0,0 +1,39 @@
+
+// var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// var char = ['rahim', 'korim', 'jobbar', 'rasel', 'sojib', 'sirat', 'panna'];
+
+// var cop = 0;
+// while (cop <= 10) {
+
+//     var result = arr[cop];
+//     console.log(result);
+
+//     cop++;
+// }
+
+// var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 200, 300];
+
+// var char = ['rahim', 'korim', 'jobbar', 'rasel', 'sojib', 'sirat', 'panna'];
+
+// var cop = 0;
+// while (cop <= arr.length) {
+
+//     var result = arr[cop];
+//     console.log(result);
+
+//     cop++;
+// }
+
+var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 200, 300];
+
+var char = ['rahim', 'korim', 'jobbar', 'rasel', 'sojib', 'sirat', 'panna'];
+
+var cop = 0;
+while (cop < char.length) {
+
+    var result = char[cop];
+    console.log(result);
+
+    cop++;
+}
